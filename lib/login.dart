6a4d27e8c82500795a4a92870d2c863c1d1d6a37
oskarfranz <oskarfranzchavez@gmail.com>
2022-10-15 19:39:53 -0500

    Update login.dart

--- lib/login.dart
+++ lib/login.dart
@@ -28,6 +28,49 @@ class _LoginState extends State<Login> {
   Widget build(BuildContext context) {
 
     return Scaffold(
+      body:
+      
+      Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top:110.0),
+            child: Image.network('https://upload.wikimedia.org/wikipedia/commons/d/db/Logo_ITESO_normal.jpg', height: 300, width: 1000,),
+          ),
+          Padding(padding:EdgeInsets.only(top:50, right:20, left:20), child: 
+            TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                prefixIcon: Icon(Icons.adjust),
+                label: Text("Nombre de usuario"),
+              ),
+            ),
+          ),
+          Padding(padding: EdgeInsets.only(top:25, right:20, left:20), child: 
+            TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                prefixIcon: Icon(Icons.donut_large),
+                label: Text("Contraseña"),
+              ),
+            ),
+          ),
+          Padding(padding: EdgeInsets.only(top:35, right:20, left:20), child: 
+            Container(
+              width: 300,
+              height: 40,
+              child: TextButton(
+                child: Text('Ingresar'),
+                onPressed: () => {},
+                style: TextButton.styleFrom(
+                  foregroundColor: Colors.white,
+                  backgroundColor: Color.fromARGB(255, 7, 29, 75)),
+                  
+              ),
+            ),
+          ),
+        ],
+      ),
+      
     );
   }
 }
